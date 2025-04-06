ca10eb3d883b423ebf83276881d64fdba351d183
Anna-Dinius <ae.dinius03@gmail.com>
2025-04-06 14:26:35 -0400

    created Change Log In Info page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,26 +1,48 @@
 import Header from './components/Header';
 import SignInUp from './components/auth/SignInUp';
 import SetUp from './components/setup/SetUp';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import ChangeLoginInfo from './components/auth/ChangeLoginInfo';
+import {
+	BrowserRouter as Router,
+	Route,
+	Routes,
+} from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <Header />
+	return (
+		<>
+			<Header />
 
-      <Router>
-        <div className="content">
-          <Routes>
-            <Route path="/" element={ SignInUp() } />
-            
-            <Route path="/step1" element={ <SetUp step={ 1 } /> } />
-            <Route path="/step2" element={ <SetUp step={ 2 } /> } />
-            <Route path="/step3" element={ <SetUp step={ 3 } /> } />
-          </Routes>
-        </div>
-      </Router>
-    </>
-  );
+			<Router>
+				<div className='content'>
+					<Routes>
+						<Route
+							path='/'
+							element={SignInUp()}
+						/>
+
+						<Route
+							path='/step1'
+							element={<SetUp step={1} />}
+						/>
+						<Route
+							path='/step2'
+							element={<SetUp step={2} />}
+						/>
+						<Route
+							path='/step3'
+							element={<SetUp step={3} />}
+						/>
+
+						<Route
+							path='/changeLogin'
+							element={<ChangeLoginInfo />}
+						/>
+					</Routes>
+				</div>
+			</Router>
+		</>
+	);
 }
 
 export default App;
