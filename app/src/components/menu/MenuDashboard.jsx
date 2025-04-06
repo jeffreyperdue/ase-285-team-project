e35d2ff7df50e850503550b8a7baf78b5d7afa6b
Jeff Perdue <perduej7@mymail.nku.edu>
2025-04-06 17:20:30 -0400

    Added functionality to +Add a Menu button in Menu Dashboard so that when clicked, a new menu card appears on Menu Dashboard

--- app/src/components/menu/MenuDashboard.jsx
+++ app/src/components/menu/MenuDashboard.jsx
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuCard from './MenuCard';
 import '../../css/MenuDashboard.css';
-
-const dummyMenus = [
-  {
-    title: 'Master Menu',
-    description: 'This menu will be shown to customers',
-  },
-];
+import deleteIcon from '../../icons/delete.png'; // Ensure this path is correct
 
 function MenuDashboard() {
+  const [menus, setMenus] = useState([
+    {
+      title: 'Master Menu',
+      description: 'This menu will be shown to customers',
+    },
+  ]);
+
+  const handleAddMenu = () => {
+    const newMenu = {
+      title: 'Untitled Menu',
+      description: 'New menu created',
+    };
+    setMenus([...menus, newMenu]);
+  };
+
   return (
     <div className="dashboard-container">
       <div className="dashboard-header">
-      <button className="button">+ Add a Menu</button>
+        <button className="button" onClick={handleAddMenu}>+ Add a Menu</button>
         <h2 className="dashboard-title">Your Menu Dashboard</h2>
       </div>
 
       <div className="menu-grid">
-        {dummyMenus.map((menu, index) => (
-          <MenuCard
-            key={index}
-            title={menu.title}
-            description={menu.description}
-            buttonLabel="View Menu"
-          />
+        {menus.map((menu, index) => (
+          <div className="menu-card-wrapper" key={index}>
+            {index !== 0 && ( // Show trash icon for all menus except index 0 (Master Menu)
+              <img src={deleteIcon} alt="Delete" className="delete-icon" />
+            )}
+            <MenuCard
+              title={menu.title}
+              description={menu.description}
+              buttonLabel="View Menu"
+            />
+          </div>
         ))}
       </div>
     </div>
