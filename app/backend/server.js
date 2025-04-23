84c02c61538df5467262fd5dcfa1d7dab4d6dd44
Anna-Dinius <ae.dinius03@gmail.com>
2025-04-22 23:29:46 -0400

    added route for cookies to server.js

--- app/backend/server.js
+++ app/backend/server.js
@@ -50,6 +50,12 @@ app.get('/', (req, res) => {
 	res.send('NomNomSafe API is running');
 });
 
+// Cookie handler
+app.get('/cookies', (req, res) => {
+	res.cookie('mycookie', 'express');
+	res.end('cookies inserted…');
+});
+
 // 404 handler
 app.use((req, res, next) => {
 	res.status(404).json({ error: 'Route not found' });
