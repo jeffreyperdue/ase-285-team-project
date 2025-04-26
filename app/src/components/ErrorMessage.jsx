51c0f07c236ec8a1a23dc7bcbd2e3ae6c102735d
Anna-Dinius <ae.dinius03@gmail.com>
2025-04-26 18:05:06 -0400

    created ErrorMessage component

---
+++ app/src/components/ErrorMessage.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import '../css/auth.scss';
+
+function ErrorMessage({ message, destination }) {
+	const navigate = useNavigate();
+
+	const nav = (event) => {
+		event.preventDefault();
+		navigate(destination);
+	};
+
+	return (
+		<div className='confirmation-container'>
+			{message}
+			<button
+				type='button'
+				onClick={nav}
+				className='button'
+			>
+				Ok
+			</button>
+		</div>
+	);
+}
+
+export default ErrorMessage;
