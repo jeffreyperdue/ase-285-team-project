0adfe6d52506a0aad144e9c9af5ee45c6d469949
Anna-Dinius <ae.dinius03@gmail.com>
2025-04-30 15:24:22 -0400

    removed file upload from step2

--- app/src/components/setup/Step2.jsx
+++ app/src/components/setup/Step2.jsx
@@ -3,88 +3,73 @@ import GenerateAllergenList from '../auth/AllergenList';
 import GenerateDietList from '../auth/DietList';
 import { useState, useEffect } from 'react';
 
-// TO DO: fix horizontal scroll issue on /step2 page
 function Step2({ updateFormData }) {
 	const [selectedAllergens, setSelectedAllergens] = useState([]);
 	const [selectedDiets, setSelectedDiets] = useState([]);
-  
+
 	// Handle allergen checkbox change
 	const handleAllergenChange = (e, allergen) => {
-	  if (e.target.checked) {
-		setSelectedAllergens(prev => [...prev, allergen]);
-	  } else {
-		setSelectedAllergens(prev => prev.filter(a => a !== allergen));
-	  }
+		if (e.target.checked) {
+			setSelectedAllergens((prev) => [...prev, allergen]);
+		} else {
+			setSelectedAllergens((prev) => prev.filter((a) => a !== allergen));
+		}
 	};
-  
+
 	// Handle diet checkbox change
 	const handleDietChange = (e, diet) => {
-	  if (e.target.checked) {
-		setSelectedDiets(prev => [...prev, diet]);
-	  } else {
-		setSelectedDiets(prev => prev.filter(d => d !== diet));
-	  }
+		if (e.target.checked) {
+			setSelectedDiets((prev) => [...prev, diet]);
+		} else {
+			setSelectedDiets((prev) => prev.filter((d) => d !== diet));
+		}
 	};
-  
+
 	// Whenever allergens or diets change, update the main formData
 	useEffect(() => {
-	  updateFormData({
-		allergens: selectedAllergens,
-		diets: selectedDiets
-	  });
+		updateFormData({
+			allergens: selectedAllergens,
+			diets: selectedDiets,
+		});
 	}, [selectedAllergens, selectedDiets]);
-  
+
 	return (
-	  <>
-		<h1>Your Menu</h1>
-  
-		<form name='setUpStep2Form' className='step2-form'>
-		  <div className='allergens'>
-			<span className='question'>
-			  Do your menu items always contain certain allergens?
-			</span>
-  
-			<div className='allergen-list'>
-			  <GenerateAllergenList
-				selectedAllergens={selectedAllergens}
-				onAllergenChange={handleAllergenChange}
-			  />
-			</div>
-		  </div>
-  
-		  <div className='diets'>
-			<span className='question'>
-			  Do you use any special preparation techniques or
-			  provide menu items for any diets?
-			</span>
-  
-			<div className='diet-list'>
-			  <GenerateDietList
-				selectedDiets={selectedDiets}
-				onDietChange={handleDietChange}
-			  />
-			</div>
-  
-			<span>
-			  For your customers' peace of mind, please upload
-			  a certificate of proof (if applicable):
-			</span>
-  
-			<div className='file-input-container'>
-			  <span className='file-input-label'>
-				Drag file to upload
-			  </span>
-  
-			  <input
-				type='file'
-				className='file-input'
-				accept='.pdf,.jpg,.png'
-			  />
-			</div>
-		  </div>
-		</form>
-	  </>
+		<>
+			<h1>Your Menu</h1>
+
+			<form
+				name='setUpStep2Form'
+				className='step2-form'
+			>
+				<div className='allergens'>
+					<span className='question'>
+						Do your menu items always contain certain allergens?
+					</span>
+
+					<div className='allergen-list'>
+						<GenerateAllergenList
+							selectedAllergens={selectedAllergens}
+							onAllergenChange={handleAllergenChange}
+						/>
+					</div>
+				</div>
+
+				<div className='diets'>
+					<span className='question'>
+						Do you use any special preparation techniques or provide menu items
+						for any diets?
+					</span>
+
+					<div className='diet-list'>
+						<GenerateDietList
+							selectedDiets={selectedDiets}
+							onDietChange={handleDietChange}
+						/>
+					</div>
+				</div>
+			</form>
+		</>
 	);
-  }
-  
-  export default Step2;
+}
+
+export default Step2;
